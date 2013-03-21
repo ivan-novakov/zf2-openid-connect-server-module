a76016bbdd308d5437b2180130af3392a3583304
Ivan Novakov <ivan.novakov@debug.cz>
2013-03-21 15:09:42 +0100

    removed debug

--- src/PhpIdServer/Authentication/AttributeFilter.php
+++ src/PhpIdServer/Authentication/AttributeFilter.php
@@ -32,7 +32,6 @@ class AttributeFilter
      */
     public function __construct(array $config, Factory $inputFilterFactory = null)
     {
-        _dump($config);
         if (null === $inputFilterFactory) {
             $inputFilterFactory = new Factory();
         }
