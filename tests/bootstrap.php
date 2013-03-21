6d45932fddf86ba91a65392153e006cc1e3f41ee
Ivan Novakov <ivan.novakov@debug.cz>
2013-03-21 14:00:06 +0100

    adjusted unit test environment

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,34 +1,11 @@
 <?php
 use PhpIdServer\Module;
-/*
-define('ZF2_PATH', realpath(__DIR__ . '/../../../vendor/zendframework/zendframework/library/') . '/');
-define('MODULE_PATH', dirname(__FILE__) . '/../');
-
-define('TESTS_ROOT', dirname(__FILE__) . '/');
-define('TMP_DIR', TESTS_ROOT . 'tmp/');
-
-define('TESTS_CONFIG_FILE', TESTS_ROOT . '_files/tests.config.php');
-
-require_once __DIR__ . '/../../../init_autoloader.php';
-
-include ZF2_PATH . 'Zend/Loader/AutoloaderFactory.php';
-Zend\Loader\AutoloaderFactory::factory(array(
-    'Zend\Loader\StandardAutoloader' => array(
-        'namespaces' => array(
-            //'Zend' => ZF2_PATH . 'Zend/', 
-            'PhpIdServer' => MODULE_PATH . 'src/PhpIdServer', 
-            'PhpIdServerTest' => TESTS_ROOT . 'PhpIdServerTest/', 
-            'MyUnit' => TESTS_ROOT . 'MyUnit'
-        )
-    )
-));
-*/
 
 define('TESTS_ROOT', dirname(__FILE__) . '/');
 define('TMP_DIR', TESTS_ROOT . 'tmp/');
 define('TESTS_CONFIG_FILE', TESTS_ROOT . '_files/tests.config.php');
 
-require __DIR__ . '/../../../vendor/autoload.php';
+require __DIR__ . '/../../../../vendor/autoload.php';
 require __DIR__ . '/../Module.php';
 
 $module = new Module();
