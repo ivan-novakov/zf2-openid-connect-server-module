6a5c841671a1f39164c35f70d57ced55412aadf1
Ivan Novakov <ivan.novakov@debug.cz>
2014-02-04 18:22:07 +0100

    reconfigured testing + "test" test

---
+++ src/InoOicServer/Util/DateTimeUtil.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace InoOicServer\Util;
+
+
+/**
+ * Date/time utility functions.
+ */
+class DateTimeUtil
+{
+
+
+    /**
+     * Creates a DateTime object based on the provided date/time string. If not set, "now" is used.
+     * For more information about date/time string formats, see http://php.net/manual/en/datetime.formats.php.
+     * 
+     * @see http://php.net/manual/en/datetime.formats.php
+     * 
+     * @param string $dateTimeString
+     * @return \DateTime
+     */
+    public function createDateTime($dateTimeString = null)
+    {
+        if (null === $dateTimeString) {
+            $dateTimeString = 'now';
+        }
+        return new \DateTime($dateTimeString);
+    }
+
+
+    /**
+     * Creates the corresponding expiration DateTime object based on the provided DateTime object and
+     * the interval.
+     * 
+     * @see http://php.net/manual/en/dateinterval.construct.php
+     * 
+     * @param \DateTime $dateTimeFrom
+     * @param string|\DateInterval $interfal
+     * @return DateTime
+     */
+    public function createExpireDateTime(\DateTime $dateTimeFrom, $interval)
+    {
+        $expireDateTime = clone $dateTimeFrom;
+        
+        if (! $interval instanceof \DateInterval) {
+            $interval = new \DateInterval($interval);
+        }
+        
+        return $expireDateTime->add($interval);
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,26 @@
 <?php
-use InoOicServer\Module;
+//use InoOicServer\Module;
 
 define('TESTS_ROOT', dirname(__FILE__) . '/');
 define('TMP_DIR', TESTS_ROOT . 'tmp/');
 define('TESTS_CONFIG_FILE', TESTS_ROOT . '_files/tests.config.php');
 
-require __DIR__ . '/../../../vendor/autoload.php';
+$autoload = null;
+$moduleAutoload = __DIR__ . '/../vendor/autoload.php';
+$appAutoload = __DIR__ . '/../../../vendor/autoload.php';
+
+if (file_exists($moduleAutoload)) {
+    $autoload = $moduleAutoload;
+} elseif (file_exists($appAutoload)) {
+    $autoload =$appAutoload;
+} else {
+    die('No autoload available');
+}
+
+//require __DIR__ . '/../../../vendor/autoload.php';
+require $autoload;
+
+/*
 require __DIR__ . '/../Module.php';
 
 $module = new Module();
@@ -18,7 +33,7 @@ $module = new Module();
         )
     )
 ));
-
+*/
 //--
 function _dump($value)
 {
