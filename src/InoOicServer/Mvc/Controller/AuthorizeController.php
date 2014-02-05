b98005d91292853f3b39cc2e1a1839a1ae370f23
Ivan Novakov <ivan.novakov@debug.cz>
2014-02-05 12:33:32 +0100

    object planning

--- src/InoOicServer/Mvc/Controller/AuthorizeController.php
+++ src/InoOicServer/Mvc/Controller/AuthorizeController.php
@@ -11,10 +11,27 @@ class AuthorizeController extends AbstractActionController
 
     public function authorizeAction()
     {
+        // create authorize request from http request
+        
+        // process authorize request
+        
+        // handle result, which can be one of these:
+        // - redirect to response endpoint (if there is valid context)
+        // - redirect to authentication
+        // - error, client known - error redirect to client
+        // - error, client not known - show HTML message directly
         return $this->getResponse();
     }
 
 
     public function responseAction()
-    {}
+    {
+        // process authorize response
+        
+        // handle result, which can be one of these:
+        // - redirect to client (valid response)
+        // - custom redirect (to registration form)
+        // - error, client known - error redirect to client
+        // - error, client not known - show HTML message directly
+    }
 }
