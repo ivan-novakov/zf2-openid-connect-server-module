96fefef537fa1f15b907480b0b7c68eaa08c5259
Ivan Novakov <ivan.novakov@debug.cz>
2014-03-26 12:29:47 +0100

    OptionsTrait

---
+++ src/InoOicServer/Util/OptionsTrait.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace InoOicServer\Util;
+
+use Zend\Stdlib\ArrayUtils;
+use Zend\Stdlib\Guard\ArrayOrTraversableGuardTrait;
+
+
+trait OptionsTrait
+{
+    
+    use ArrayOrTraversableGuardTrait;
+
+    /**
+     * @var Options
+     */
+    protected $options;
+
+
+    public function setOptions($options)
+    {
+        $this->guardForArrayOrTraversable($options, 'Options');
+        
+        $options = ArrayUtils::iteratorToArray($options, true);
+        
+        if (isset($this->defaultOptions) && is_array($this->defaultOptions)) {
+            $options = ArrayUtils::merge($this->defaultOptions, $options);
+        }
+        
+        $this->options = $this->createOptions($options);
+    }
+
+
+    public function getOptions()
+    {
+        if (! $this->options instanceof Options) {
+            $this->options = $this->createOptions();
+        }
+        return $this->options;
+    }
+
+
+    public function getOption($optionName, $defaultValue = null)
+    {
+        return $this->getOptions()->get($optionName, $defaultValue);
+    }
+
+
+    protected function createOptions(array $options = array())
+    {
+        return new Options($options);
+    }
+}
---
+++ tests/unit/InoOicServerTest/Util/OptionsTraitTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace InoOicServer\Util\OptionsTrait;
+
+use InoOicServer\Util\Options;
+
+
+class OptionsTraitTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $trait;
+
+
+    public function setUp()
+    {
+        $this->trait = $this->getObjectForTrait('InoOicServer\Util\OptionsTrait');
+    }
+
+
+    public function testSetOptionsWithInvalidArgument()
+    {
+        $this->setExpectedException('Zend\Stdlib\Exception\InvalidArgumentException', 'Options must be an array or Traversable');
+        
+        $this->trait->setOptions('invalid');
+    }
+
+
+    public function testSetOptionsWithArray()
+    {
+        $options = array(
+            'foo1' => 'bar1',
+            'foo2' => 'bar2'
+        );
+        
+        $this->trait->setOptions($options);
+        
+        $this->assertEquals($options, (array) $this->trait->getOptions());
+    }
+
+
+    public function testSetOptionsWithOptions()
+    {
+        $options = new Options(array(
+            'foo1' => 'bar1',
+            'foo2' => 'bar2'
+        ));
+        
+        $this->trait->setOptions($options);
+        
+        $this->assertEquals($options, $this->trait->getOptions());
+    }
+
+
+    public function testSetOptionsWithDefaultOptions()
+    {
+        $this->trait->defaultOptions = array(
+            'foo1' => 'bar1',
+            'foo2' => 'bar2',
+            'foo3' => 'bar3'
+        );
+        
+        $options = array(
+            'foo2' => 'bar21'
+        );
+        
+        $this->trait->setOptions($options);
+        
+        $expected = array(
+            'foo1' => 'bar1',
+            'foo2' => 'bar21',
+            'foo3' => 'bar3'
+        );
+        
+        $this->assertSame($expected, (array) $this->trait->getOptions());
+    }
+
+
+    public function testGetOption()
+    {
+        $this->assertNull($this->trait->getOption('foo'));
+        
+        $this->trait->setOptions(array(
+            'foo' => 'bar'
+        ));
+        
+        $this->assertSame('bar', $this->trait->getOption('foo'));
+    }
+
+
+    public function testGetOptionWithDefaultValue()
+    {
+        $this->assertSame('def', $this->trait->getOption('foo', 'def'));
+    }
+}
+
