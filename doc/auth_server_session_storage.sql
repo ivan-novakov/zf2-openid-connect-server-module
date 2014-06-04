15926abaec1677334bd87479ebd41b5cab05bb69
Ivan Novakov <ivan.novakov@debug.cz>
2014-06-04 15:03:33 +0200

    updated db schema

--- doc/auth_server_session_storage.sql
+++ doc/auth_server_session_storage.sql
@@ -1,99 +1,110 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 
 -- -----------------------------------------------------
--- Table `session`
+-- Table `oic_server`.`auth_session`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `session` ;
+DROP TABLE IF EXISTS `oic_server`.`auth_session` ;
 
-CREATE  TABLE IF NOT EXISTS `session` (
-  `id` CHAR(40) NOT NULL ,
-  `user_id` VARCHAR(255) NOT NULL ,
-  `create_time` DATETIME NOT NULL ,
-  `modify_time` DATETIME NOT NULL ,
-  `expiration_time` DATETIME NOT NULL ,
-  `authentication_time` DATETIME NOT NULL ,
-  `authentication_method` VARCHAR(32) NOT NULL ,
-  `user_data` TEXT NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_general_ci;
+CREATE TABLE IF NOT EXISTS `oic_server`.`auth_session` (
+  `id` VARCHAR(32) NOT NULL,
+  `method` VARCHAR(45) NOT NULL,
+  `create_time` DATETIME NOT NULL,
+  `expiration_time` DATETIME NOT NULL,
+  `user_id` VARCHAR(45) NOT NULL,
+  `user_data` TEXT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `authorization_code`
+-- Table `oic_server`.`session`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `authorization_code` ;
+DROP TABLE IF EXISTS `oic_server`.`session` ;
 
-CREATE  TABLE IF NOT EXISTS `authorization_code` (
-  `code` CHAR(40) NOT NULL ,
-  `session_id` CHAR(40) NOT NULL ,
-  `issue_time` DATETIME NOT NULL ,
-  `expiration_time` DATETIME NOT NULL ,
-  `client_id` VARCHAR(255) NOT NULL ,
-  `scope` VARCHAR(255) NOT NULL ,
-  PRIMARY KEY (`code`) ,
-  INDEX `fk_authorization_code_session` (`session_id` ASC) ,
+CREATE TABLE IF NOT EXISTS `oic_server`.`session` (
+  `id` VARCHAR(32) NOT NULL,
+  `auth_session_id` VARCHAR(32) NOT NULL,
+  `create_time` DATETIME NOT NULL,
+  `modify_time` DATETIME NOT NULL,
+  `expiration_time` DATETIME NOT NULL,
+  `nonce` VARCHAR(64) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_session_auth_session1_idx` (`auth_session_id` ASC),
+  CONSTRAINT `fk_session_auth_session1`
+    FOREIGN KEY (`auth_session_id`)
+    REFERENCES `oic_server`.`auth_session` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `oic_server`.`authorization_code`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `oic_server`.`authorization_code` ;
+
+CREATE TABLE IF NOT EXISTS `oic_server`.`authorization_code` (
+  `code` VARCHAR(32) NOT NULL,
+  `session_id` VARCHAR(32) NOT NULL,
+  `issue_time` DATETIME NOT NULL,
+  `expiration_time` DATETIME NOT NULL,
+  `client_id` VARCHAR(255) NOT NULL,
+  `scope` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`code`),
+  INDEX `fk_authorization_code_session_idx` (`session_id` ASC),
   CONSTRAINT `fk_authorization_code_session`
-    FOREIGN KEY (`session_id` )
-    REFERENCES `session` (`id` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_general_ci;
+    FOREIGN KEY (`session_id`)
+    REFERENCES `oic_server`.`session` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `access_token`
+-- Table `oic_server`.`access_token`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `access_token` ;
+DROP TABLE IF EXISTS `oic_server`.`access_token` ;
 
-CREATE  TABLE IF NOT EXISTS `access_token` (
-  `token` CHAR(40) NOT NULL ,
-  `session_id` CHAR(40) NOT NULL ,
-  `issue_time` DATETIME NOT NULL ,
-  `expiration_time` DATETIME NOT NULL ,
-  `client_id` VARCHAR(255) NOT NULL ,
-  `type` VARCHAR(32) NOT NULL DEFAULT 'bearer' ,
-  `scope` VARCHAR(255) NOT NULL ,
-  PRIMARY KEY (`token`) ,
-  INDEX `fk_access_token_session1` (`session_id` ASC) ,
+CREATE TABLE IF NOT EXISTS `oic_server`.`access_token` (
+  `token` VARCHAR(32) NOT NULL,
+  `session_id` VARCHAR(32) NOT NULL,
+  `issue_time` DATETIME NOT NULL,
+  `expiration_time` DATETIME NOT NULL,
+  `client_id` VARCHAR(255) NOT NULL,
+  `type` VARCHAR(32) NOT NULL DEFAULT 'bearer',
+  `scope` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`token`),
+  INDEX `fk_access_token_session1_idx` (`session_id` ASC),
   CONSTRAINT `fk_access_token_session1`
-    FOREIGN KEY (`session_id` )
-    REFERENCES `session` (`id` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_general_ci;
+    FOREIGN KEY (`session_id`)
+    REFERENCES `oic_server`.`session` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `refresh_token`
+-- Table `oic_server`.`refresh_token`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `refresh_token` ;
+DROP TABLE IF EXISTS `oic_server`.`refresh_token` ;
 
-CREATE  TABLE IF NOT EXISTS `refresh_token` (
-  `token` CHAR(40) NOT NULL ,
-  `access_token` CHAR(40) NOT NULL ,
-  `issue_time` DATETIME NOT NULL ,
-  `expiration_time` DATETIME NOT NULL ,
-  `client_id` VARCHAR(255) NOT NULL ,
-  PRIMARY KEY (`token`) ,
-  INDEX `fk_refresh_token_access_token1` (`access_token` ASC) ,
+CREATE TABLE IF NOT EXISTS `oic_server`.`refresh_token` (
+  `token` VARCHAR(32) NOT NULL,
+  `access_token` VARCHAR(32) NOT NULL,
+  `issue_time` DATETIME NOT NULL,
+  `expiration_time` DATETIME NOT NULL,
+  `client_id` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`token`),
+  INDEX `fk_refresh_token_access_token1_idx` (`access_token` ASC),
   CONSTRAINT `fk_refresh_token_access_token1`
-    FOREIGN KEY (`access_token` )
-    REFERENCES `access_token` (`token` )
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_general_ci;
-
+    FOREIGN KEY (`access_token`)
+    REFERENCES `oic_server`.`access_token` (`token`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
