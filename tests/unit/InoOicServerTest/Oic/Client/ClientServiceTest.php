ce125f3084688d2e0ada87589c6f7c557ac663c8
Ivan Novakov <ivan.novakov@debug.cz>
2014-07-21 19:10:14 +0200

    fixed test

--- tests/unit/InoOicServerTest/Oic/Client/ClientServiceTest.php
+++ tests/unit/InoOicServerTest/Oic/Client/ClientServiceTest.php
@@ -1,23 +1,19 @@
 <?php
-
 namespace InoOicServerTest\Oic\Client;
 
 use InoOicServer\Oic\Client\ClientService;
 use InoOicServer\Oic\Client\Client;
 use InoOicServer\Oic\Authorize\AuthorizeRequest;
 
-
 class ClientServiceTest extends \PHPUnit_Framework_TestCase
 {
 
     protected $service;
 
-
     public function setUp()
     {
         $this->service = new ClientService($this->createMapperMock());
     }
-
 
     public function testSetGetMapper()
     {
@@ -26,38 +22,38 @@ class ClientServiceTest extends \PHPUnit_Framework_TestCase
         $this->assertSame($mapper, $this->service->getClientMapper());
     }
 
-
     public function testFetchClientWithRedirectUriMismatch()
     {
         $this->setExpectedException('InoOicServer\Oic\Client\Exception\RedirectUriMismatchException');
-
+        
         $clientId = 'testclient';
         $redirectUri = 'http://redirect';
-
+        
         $client = $this->createClientMock();
         $client->expects($this->once())
             ->method('hasRedirectUri')
             ->with($redirectUri)
             ->will($this->returnValue(false));
-
+        
         $mapper = $this->createMapperMock();
         $mapper->expects($this->once())
             ->method('getClientById')
             ->with($clientId)
             ->will($this->returnValue($client));
-
+        
         $this->service->setClientMapper($mapper);
         $this->service->fetchClient($clientId, $redirectUri);
     }
 
-
     public function testFetchClientFromAuthorizeRequestWithNotFound()
     {
+        $this->setExpectedException('InoOicServer\Oic\Client\Exception\UnknownClientException', 'Unknown client');
+        
         $clientId = 'testclient';
-
+        
         $request = new AuthorizeRequest();
         $request->setClientId($clientId);
-
+        
         $service = $this->getMockBuilder('InoOicServer\Oic\Client\ClientService')
             ->disableOriginalConstructor()
             ->setMethods(array(
@@ -68,68 +64,69 @@ class ClientServiceTest extends \PHPUnit_Framework_TestCase
             ->method('fetchClient')
             ->with($clientId)
             ->will($this->returnValue(null));
-
-        $this->assertNull($service->fetchClientFromAuthorizeRequest($request));
-    }
-    public function testFetchClientFromAuthorizeRequestWithMissingRedirectUri()
-    {
-        $this->setExpectedException('InoOicServer\Oic\Client\Exception\MissingRedirectUriException', 'No redirect URI in authorize request');
-
-        $clientId = 'testclient';
-
-        $client = new Client();
-        $client->setId($clientId);
-
-        $request = new AuthorizeRequest();
-        $request->setClientId($clientId);
-
-        $service = $this->getMockBuilder('InoOicServer\Oic\Client\ClientService')
-        ->disableOriginalConstructor()
-        ->setMethods(array(
-            'fetchClient'
-        ))
-        ->getMock();
-
-        $service->expects($this->once())
-        ->method('fetchClient')
-        ->with($clientId)
-        ->will($this->returnValue($client));
-
+        
         $service->fetchClientFromAuthorizeRequest($request);
     }
 
-    public function testFetchClientFromAuthorizeRequestWithRedirectUriMismatch()
+    public function testFetchClientFromAuthorizeRequestWithMissingRedirectUri()
     {
-        $this->setExpectedException('InoOicServer\Oic\Client\Exception\RedirectUriMismatchException', 'Invalid redirect URI');
-
+        $this->setExpectedException('InoOicServer\Oic\Client\Exception\MissingRedirectUriException', 'No redirect URI in authorize request');
+        
         $clientId = 'testclient';
-        $redirectUri = 'https://redirect';
-
+        
         $client = new Client();
         $client->setId($clientId);
-        $client->setRedirectUris(array(
-            $redirectUri
-        ));
-
+        
         $request = new AuthorizeRequest();
         $request->setClientId($clientId);
-        $request->setRedirectUri('https://invalid/redirect');
-
+        
         $service = $this->getMockBuilder('InoOicServer\Oic\Client\ClientService')
             ->disableOriginalConstructor()
             ->setMethods(array(
             'fetchClient'
         ))
             ->getMock();
-
+        
         $service->expects($this->once())
             ->method('fetchClient')
             ->with($clientId)
             ->will($this->returnValue($client));
-
+        
         $service->fetchClientFromAuthorizeRequest($request);
     }
 
+    public function testFetchClientFromAuthorizeRequestWithRedirectUriMismatch()
+    {
+        $this->setExpectedException('InoOicServer\Oic\Client\Exception\RedirectUriMismatchException', 'Invalid redirect URI');
+        
+        $clientId = 'testclient';
+        $redirectUri = 'https://redirect';
+        
+        $client = new Client();
+        $client->setId($clientId);
+        $client->setRedirectUris(array(
+            $redirectUri
+        ));
+        
+        $request = new AuthorizeRequest();
+        $request->setClientId($clientId);
+        $request->setRedirectUri('https://invalid/redirect');
+        
+        $service = $this->getMockBuilder('InoOicServer\Oic\Client\ClientService')
+            ->disableOriginalConstructor()
+            ->setMethods(array(
+            'fetchClient'
+        ))
+            ->getMock();
+        
+        $service->expects($this->once())
+            ->method('fetchClient')
+            ->with($clientId)
+            ->will($this->returnValue($client));
+        
+        $service->fetchClientFromAuthorizeRequest($request);
+    }
+    
     /*
      *
      */
@@ -139,10 +136,9 @@ class ClientServiceTest extends \PHPUnit_Framework_TestCase
     protected function createMapperMock()
     {
         $mapper = $this->getMock('InoOicServer\Oic\Client\Mapper\MapperInterface');
-
+        
         return $mapper;
     }
-
 
     /**
      * @return \PHPUnit_Framework_MockObject_MockObject
@@ -150,7 +146,7 @@ class ClientServiceTest extends \PHPUnit_Framework_TestCase
     protected function createClientMock()
     {
         $client = $this->getMock('InoOicServer\Oic\Client\Client');
-
+        
         return $client;
     }
 }
